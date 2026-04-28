de467dfbf69eb98cfc4a48e0eddab144e30c43e6
Mohamed <mohamedlgrindi@gmail.com>
2026-04-29 00:25:41 +0100

    add a println

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from TP Java Jenkins Pipeline");
+        System.out.println("Hello from TP Java Jenkins ");
     }
 }
